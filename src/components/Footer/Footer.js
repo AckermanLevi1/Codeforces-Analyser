e79d3a8f6fc21cdb95c2eab7cbdc0c6517480813
AckermanLevi1 <aryan.bhargava.mec21@itbhu.ac.in>
2024-08-14 15:09:26 +0530

    added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import './Footer.css';
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <span>
+            &copy; <a href="https://codeforces.com/profile/kingkohli"  target="_blank">Aryan Bhargava</a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
